94c2491ca3871444e981a1e5e1b0abc174f7edf5
Shaun <shaun.carmody@live.co.uk>
2018-09-05 16:33:55 +0100

    log url

--- routes/controllers/render.js
+++ routes/controllers/render.js
@@ -1,7 +1,7 @@
 const tabBar = 'Streetwised';
 
 module.exports.account = (req, res) => {
-  console.log(req.header);
+  console.log(req.header.host);
   let google = null;
   let mixpanel = '_partials/analytics/dev/mixpanel.html';
   if (req.header.host === 'streetwised.com') {
