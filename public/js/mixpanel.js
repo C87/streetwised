861821e3d4afbd2467e42b8e234434b02f0a558b
Shaun <shaun.carmody@live.co.uk>
2018-09-05 15:27:36 +0100

    run analytics on production only

--- public/js/mixpanel.js
+++ public/js/mixpanel.js
@@ -3,35 +3,38 @@
 console.log(window.location.host);
 console.log(window.location.hostname);
 
-const analytics = {};
+if (window.location.hostname === 'streetwised.com') {
+  console.log('works');
+  const analytics = {};
 
-analytics.signup = (user, name, email) => {
-  mixpanel.identify(user);
-  mixpanel.track('User Signup');
-  mixpanel.people.set({
-    $name: name,
-    $email: email,
-    username: user,
-  });
-};
+  analytics.signup = (user, name, email) => {
+    mixpanel.identify(user);
+    mixpanel.track('User Signup');
+    mixpanel.people.set({
+      $name: name,
+      $email: email,
+      username: user,
+    });
+  };
 
-analytics.login = (user) => {
-  mixpanel.identify(user);
-  mixpanel.track('User Login');
-};
+  analytics.login = (user) => {
+    mixpanel.identify(user);
+    mixpanel.track('User Login');
+  };
 
-analytics.question = (t, ql, tl) => {
-  mixpanel.track('Question Asked', {
-    Tag: t,
-    'Question Length': ql,
-    'Tag Length': tl,
-  });
-};
+  analytics.question = (t, ql, tl) => {
+    mixpanel.track('Question Asked', {
+      Tag: t,
+      'Question Length': ql,
+      'Tag Length': tl,
+    });
+  };
 
-analytics.response = (t, ql, rl) => {
-  mixpanel.track('Question Response', {
-    Tag: t,
-    'Question Length': ql,
-    'Response Length': rl,
-  });
-};
+  analytics.response = (t, ql, rl) => {
+    mixpanel.track('Question Response', {
+      Tag: t,
+      'Question Length': ql,
+      'Response Length': rl,
+    });
+  };
+}
