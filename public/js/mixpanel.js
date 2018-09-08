ac7f8af46762cb1e1bf7be5e0818e615e4c5c83d
Shaun <shaun.carmody@live.co.uk>
2018-09-08 04:24:14 +0100

    adjust mixpanel

--- public/js/mixpanel.js
+++ public/js/mixpanel.js
@@ -3,32 +3,32 @@
 const analytics = {};
 
 analytics.signup = (user, name, email) => {
-  // mixpanel.identify(user);
-  // mixpanel.track('User Signup');
-  // mixpanel.people.set({
-  //   $name: name,
-  //   $email: email,
-  //   username: user,
-  // });
+  mixpanel.identify(user);
+  mixpanel.track('User Signup');
+  mixpanel.people.set({
+    $name: name,
+    $email: email,
+    username: user,
+  });
 };
 
 analytics.login = (user) => {
-  // mixpanel.identify(user);
-  // mixpanel.track('User Login');
+  mixpanel.identify(user);
+  mixpanel.track('User Login');
 };
 
 analytics.question = (t, ql, tl) => {
-  // mixpanel.track('Question Asked', {
-  //   Tag: t,
-  //   'Question Length': ql,
-  //   'Tag Length': tl,
-  // });
+  mixpanel.track('Question Asked', {
+    Tag: t,
+    'Question Length': ql,
+    'Tag Length': tl,
+  });
 };
 
 analytics.response = (t, ql, rl) => {
-  // mixpanel.track('Question Response', {
-  //   Tag: t,
-  //   'Question Length': ql,
-  //   'Response Length': rl,
-  // });
+  mixpanel.track('Question Response', {
+    Tag: t,
+    'Question Length': ql,
+    'Response Length': rl,
+  });
 };
